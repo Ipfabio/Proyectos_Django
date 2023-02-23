45bb3b8f5b0b7e5ceaf761a81cb26887730027bc
Ip.fabio <95320507+Ipfabio@users.noreply.github.com>
2023-02-23 12:24:41 -0300

    Update settings.py

--- Login_Logout/login/settings.py
+++ Login_Logout/login/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rth%n^yks&l&+81pdk(cw0x_yukmt&h!i*2ce6u-$ad-@x(ede'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
